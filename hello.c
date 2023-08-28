66142b8d76c846a7a288d5bac8283aa9070b2e41
zaharbari <zbari2@huskers.unl.edu>
2023-08-28 11:14:21 -0700

    added name

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 /**
+<<<<<<< HEAD
  * Author: Eleanor Krause
  *         ekrause5@huskers.unl.edu
+=======
+ *  Author: Zahar Bari
+>>>>>>> e7578eb (add partner code)
  * Date: 2023/08/24
  *
  * A program that prints your name and major
@@ -11,8 +15,13 @@
 
 int main(int argc, char **argv) {
 
+<<<<<<< HEAD
   printf("Eleanor Krause!\n");
   printf("My major is Computer Science!\n");
+=======
+   printf("Zahar Bari!\n");
+   printf("Computer Science!!\n");
+>>>>>>> e7578eb (add partner code)
 
   return 0;
 }
